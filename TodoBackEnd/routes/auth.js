fb838b7a00ed9fe130b5453f0e41c01296c72637
vishal739 <136896825+vishal739@users.noreply.github.com>
2024-01-24 16:46:34 +0530

    Delete TodoBackEnd directory

--- TodoBackEnd/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const routes= express.Router();
-const authController= require("../controller/auth");
-routes
-    .post('/signup',authController.signup)
-    .post('/login',authController.login);
-
-exports.authRouter= routes;
